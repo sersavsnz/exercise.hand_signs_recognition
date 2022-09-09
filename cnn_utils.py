ec8efca6b68ab963fe5eac24c1bf015a2f0c2673
sersavsnz <spsavin@gmail.com>
2022-09-09 20:25:37 +0700

    first commit

--- cnn_utils.py
+++ cnn_utils.py
@@ -1,14 +1,14 @@
 import numpy as np
 import h5py
 
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_signs.h5', "r")
+def load_dataset(path):
+    train_dataset = h5py.File(f'{path}/train_signs.h5', "r")
     # your train set features
     train_set_x_orig = np.array(train_dataset["train_set_x"][:])
     train_set_y_orig = np.array(
         train_dataset["train_set_y"][:])  # your train set labels
 
-    test_dataset = h5py.File('datasets/test_signs.h5', "r")
+    test_dataset = h5py.File(f'{path}/test_signs.h5', "r")
     # your test set features
     test_set_x_orig = np.array(test_dataset["test_set_x"][:])
     test_set_y_orig = np.array(
